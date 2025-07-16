211b3c341b2ae5c5f42141fd933d9b53753d9fe9
Xbectordash <vectordash755@gmail.com>
2025-07-16 17:40:30 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/waveauth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.waveauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/custom_painter/wave_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WaveScreen extends StatefulWidget {
+  const WaveScreen({super.key});
+
+  @override
+  State<WaveScreen> createState() => _WaveScreenState();
+}
+
+class _WaveScreenState extends State<WaveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+
+    );
+  }
+}
